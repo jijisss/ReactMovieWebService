8d43f5772f6be91399960f06a93c8629f59d3afc
jieun <sjy990302@naver.com>
2022-09-20 17:48:14 +0900

    start-react
    create-react-app 생성한 start-react 파일!

---
+++ start-react/src/App.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [ counter, setValue ] = useState(0);
+  const [keyWord, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyWord]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter changes.");
+  }, [keyWord, counter]);
+  return (
+    <div>
+      <input 
+      value={keyWord} 
+      onChange={onChange} 
+      type="text" 
+      placeholder="Search here..." 
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me!</button>
+    </div>
+  );
+}
+
+export default App;
